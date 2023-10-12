ef5367668191fdf7806f56241579add0acd467fb
박장현/janghyeon park <63183021+park-janghyeon@users.noreply.github.com>
2023-10-12 10:23:25 +0900

    my first spring project

---
+++ 박장현/src/test/java/cau/gdsc/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cau.gdsc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

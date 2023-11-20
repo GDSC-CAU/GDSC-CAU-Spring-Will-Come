6b9add422e6418a50ba6029a5508769e42626738
Urchinode <vrdhan212@gmail.com>
2023-11-21 00:34:00 +0900

    update: `Article` CRUD apis with http status

--- sinhan/src/main/java/cau/gdsc/controller/ArticleController.java
+++ sinhan/src/main/java/cau/gdsc/controller/ArticleController.java
@@ -1,22 +1,53 @@
 package cau.gdsc.controller;
 
 import cau.gdsc.domain.Article;
+import cau.gdsc.dto.ArticleReqDto;
 import cau.gdsc.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/article")
 public class ArticleController {
-    @Autowired
-    private ArticleService articleService;
+    private final ArticleService articleService;
 
-    @GetMapping
-    public List<Article> getArticles() {
-        return articleService.getArticles();
+    @Autowired
+    public ArticleController(ArticleService articleService){
+        this.articleService = articleService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Article>> getArticles() {
+        return new ResponseEntity<>(articleService.getArticles(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Article> getArticleById(@PathVariable Long id){
+        return new ResponseEntity<>(articleService.getArticleById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Article> createArticle(@RequestBody ArticleReqDto articleReqDto){
+        return new ResponseEntity<>(articleService.createArticle(articleReqDto), HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable Long id, @RequestBody ArticleReqDto articleReqDto){
+        return new ResponseEntity<>(articleService.updateArticle(id, articleReqDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteArticleById(@PathVariable Long id){
+        try{
+            articleService.deleteArticleById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (EntityNotFoundException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }

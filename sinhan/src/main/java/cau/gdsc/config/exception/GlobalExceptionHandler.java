31f0779a0646f0107f1225631a0d816149d811c9
Urchinode <vrdhan212@gmail.com>
2023-11-23 22:32:27 +0900

    fix: return exact status when fail

--- sinhan/src/main/java/cau/gdsc/config/exception/GlobalExceptionHandler.java
+++ sinhan/src/main/java/cau/gdsc/config/exception/GlobalExceptionHandler.java
@@ -1,13 +1,16 @@
 package cau.gdsc.config.exception;
 
 import cau.gdsc.config.api.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
-    public ApiResponse<?> handleException(BaseException e){
-        return ApiResponse.fail(e.getResponseCode(), null);
+    public ResponseEntity<ApiResponse> handleException(BaseException e){
+        return ResponseEntity
+                .status(e.getResponseCode().getHttpStatus())
+                .body(ApiResponse.fail(e.getResponseCode(), e.getMessage()));
     }
 }

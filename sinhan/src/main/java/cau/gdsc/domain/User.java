622f0c53935197d44e232138d2ead410dc3ee1f0
vrdhan212 <vrdhan212@gmail.com>
2023-12-26 17:49:46 +0900

    update: set `User` entity to extend `UserDetails`

--- sinhan/src/main/java/cau/gdsc/domain/User.java
+++ sinhan/src/main/java/cau/gdsc/domain/User.java
@@ -2,14 +2,18 @@ package cau.gdsc.domain;
 
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
-public class User {
+public class User implements UserDetails { // 스프링 시큐리티 UserDetails 인터페이스를 구현
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,8 +40,44 @@ public class User {
     }
 
     // 필요한 update 정보만 받아와 수정
-    public void update(int height, int weight){
+    public void update(int height, int weight) {
         this.height = height;
         this.weight = weight;
+    }
+
+    // 스프링 시큐리티에서 사용하는 메소드들
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
